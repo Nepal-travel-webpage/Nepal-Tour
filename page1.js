f094a1ce79a0318ed691763aea0bcc37f0ea768b
amankanwal <amankanwal1221@gmail.com>
2026-02-14 12:01:00 +0530

    itinerary added new assets

--- page1.js
+++ page1.js
@@ -1,14 +1,12 @@
+const slides = document.querySelectorAll(".hero-slide");
+let current = 0;
 
-  const slides = document.querySelectorAll(".hero-slide");
-  let current = 0;
+function showSlide(index) {
+  slides.forEach((slide) => slide.classList.remove("active"));
+  slides[index].classList.add("active");
+}
 
-  function showSlide(index) {
-    slides.forEach(slide => slide.classList.remove("active"));
-    slides[index].classList.add("active");
-  }
-
-  setInterval(() => {
-    current = (current + 1) % slides.length;
-    showSlide(current);
-  }, 5000); // change image every 5 seconds
-
+setInterval(() => {
+  current = (current + 1) % slides.length;
+  showSlide(current);
+}, 5000); // change image every 5 seconds
